f002ddebd93e08dd4d93181696305e033a10351c
Uttaran Das <uttarandas501@gmail.com>
2024-05-14 22:52:35 +0530

    About section

--- frontend/src/components/Aboutpage.jsx
+++ frontend/src/components/Aboutpage.jsx
@@ -65,7 +65,20 @@ const Aboutpage = () => {
             Information Technology, Lucknow (IIITL) pursuing Computer Science
             and Artificial Intelligence.
           </p>
-            
+        </div>
+        <div className="flex flex-col items-center justify-center bg-white rounded-lg shadow-lg px-8 py-6 m-4 w-full">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/000/350/111/non_2x/vector-male-student-icon.jpg"
+            alt="Photo of Student 1"
+            className="w-32 h-32 rounded-full object-cover mb-4"
+          />
+          <h2 className="text-2xl font-bold mb-2">Navya Yadav</h2>
+          <h2 className="text-xl font-bold mb-2">LCI2020046</h2>
+          <p className="text-gray-700 text-center mb-4">
+            Currently an undergraduate student at Indian Institute of
+            Information Technology, Lucknow (IIITL) pursuing Computer Science
+            and Artificial Intelligence.
+          </p>
         </div>
       </div>
     </>
